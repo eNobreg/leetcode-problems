96caf8431ed154da574c78e8532313ebee212420
eNobreg <ezra.nobrega@outlook.com>
2021-08-11 17:40:26 -0400

    [Update] First easy problem

---
+++ isPalindrome/isPalindrome/isPalindrome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public bool IsPalindrome(int x) {
+        string number = x.ToString();
+        
+        for(int i = 0; i < number.Length / 2; i++)
+        {
+            if (number[i] != number[number.Length - i - 1])
+                return false;
+        }
+        return true;
+    }
+}
+
