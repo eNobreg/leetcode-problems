f7f29189af1a8e6c9efd73062dbf706af90594e5
eNobreg <ezra.nobrega@outlook.com>
2021-08-11 20:19:17 -0400

    [Update] Slow C# solution, want to fix later to be at least 50% faster then other solutions

---
+++ validParantheses/validParantheses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+class Program
+{
+	public bool IsValid(string s) {
+		Stack<char> opened = new Stack<char>();
+		for (int i = 0; i< s.Count(); i++)
+		{
+			if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+				opened.Push(s[i]);
+			else if (opened.Count != 0)
+			{
+				if (s[i] == ')')
+				{
+					char ch = opened.Peek();
+					opened.Pop();
+					if (ch != '(')
+						return false;
+				}
+				if (s[i] == ']')
+				{
+					char ch = opened.Peek();
+					opened.Pop();
+					if (ch != '[')
+						return false;
+				}
+				if (s[i] == '}')
+				{
+					char ch = opened.Peek();
+					opened.Pop();
+				if (ch != '{')
+					return false;
+				}
+			}   
+			else
+				return false;
+		}
+		if (opened.Count == 0)
+			return true;
+		else
+			return false;
+	}
+}
