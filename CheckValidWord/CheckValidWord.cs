bccd519b93b90ffb6e45ff3eebd923f36f167385
eNobreg <ezra.nobrega@outlook.com>
2021-08-11 22:40:49 -0400

    [Update] Adding another slow method for doing this, will improve later

---
+++ CheckValidWord/CheckValidWord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+		Console.WriteLine(IsValid("aabcbc"));
+    }
+
+	public static bool IsValid(string s) {
+        
+        if (s[0] != 'a' || s[s.Count() - 1] != 'c')
+        {
+            return false;
+        }
+        
+        while(s.Count() != 0)
+        {
+            int last = s.LastIndexOf("abc");
+			Console.WriteLine(last);
+            if(last != -1)
+            {
+                s = s.Remove(last, 3);
+				Console.WriteLine(s);
+            }
+            else if (s.Count() != 0 && last == -1)
+                return false;
+        }
+        return true;
+    }
+}
+
